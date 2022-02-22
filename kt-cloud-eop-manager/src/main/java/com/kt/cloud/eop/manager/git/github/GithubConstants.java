0110c268289940af438d62f7b0502e6e5b783d23
Javis <javischen9548@foxmail.com>
2022-02-22 22:46:42 +0800

    u

---
+++ kt-cloud-eop-manager/src/main/java/com/kt/cloud/eop/manager/git/github/GithubConstants.java
@@ -0,0 +1,10 @@
+package com.kt.cloud.eop.manager.git.github;
+
+/**
+ * 码云
+ */
+public interface GithubConstants {
+
+    String CREATE_REPOS_URI = "https://api.github.com/api/v5/user/repos";
+    String GET_TOKEN_URI = "https://api.github.com/oauth/token";
+}
