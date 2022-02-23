dd298fc3cdbae1c744b3cacfeccd5593808e8c00
Javis <javischen9548@foxmail.com>
2022-02-23 18:41:41 +0800

    feature:完成功能

---
+++ kt-cloud-eop-manager/src/main/java/com/kt/cloud/eop/manager/git/gitee/request/GiteeCreateReposRequest.java
@@ -0,0 +1,12 @@
+
+package com.kt.cloud.eop.manager.git.gitee.request;
+
+import com.kt.cloud.eop.manager.git.GitCreateReposRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class GiteeCreateReposRequest extends GitCreateReposRequest {
+
+}
