478d98d148ee7d84c55deab7fbc04dc496fd99bc
Javis <javischen9548@foxmail.com>
2022-02-25 18:19:54 +0800

    u

--- kt-cloud-eop-start/src/main/java/com/kt/cloud/eop/Application.java
+++ kt-cloud-eop-start/src/main/java/com/kt/cloud/eop/Application.java
@@ -1,5 +1,6 @@
 package com.kt.cloud.eop;
 
+import com.kt.component.web.config.CloudAppConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(scanBasePackages = "com.kt.cloud.**")
 @MapperScan("com.kt.cloud.eop.dao.mapper")
 @EnableDiscoveryClient
-public class Application {
+public class Application extends CloudAppConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
